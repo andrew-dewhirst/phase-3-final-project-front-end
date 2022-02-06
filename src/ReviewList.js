94ea562e54502e3ed133cc323d1d763a67d9a467
Andrew Dewhirst <andrew.dewhirst8@gmail.com>
2022-02-05 19:27:01 -0500

    Updated individual reviews page with rating, location, and activities

--- src/ReviewList.js
+++ src/ReviewList.js
@@ -2,13 +2,20 @@ import React, { useEffect, useState } from "react";
 
 function ReviewList({ trip, reviews }) {
 
+  console.log(trip)
   console.log(reviews)
 
   return (
     <div>
-      <h3>{trip.location}</h3>
+      <h3>Reviews: {trip.location}</h3>
       {reviews.map((review) => (
-          <ul key={review.id}>{review.activities_completed}</ul>
+        <div key={review.id}>
+          <ul>Activities Completed: {review.activities_completed}</ul>
+          <ul>Trip Comments: {review.comments}</ul>
+          <ul>Review Submitted: {review.created_at.slice(0,-14)}</ul>
+          <h4>Trip Rating: {review.rating}</h4>
+          <button>Remove Review</button>
+        </div>
       ))}
     </div>
   );
